394132062dc57f1aeaf0049650088a568c12b5c5
InwSaKodBeiw007 <gamekung2707@gmail.com>
2025-09-03 00:45:45 +0700

    Just reaziled some easter egg

--- inoutTestTitanic.py
+++ inoutTestTitanic.py
@@ -29,3 +29,45 @@ print(model_train.predict(test_data))
 # print(type(test_data))
 # print(test_data)
 # print(train_target.head())
+# print(train_data.head())
+# print(type(train_data.head()))
+
+for i,zeries in train_data.head().iterrows():
+    # model_train.predict([zeries])
+    print(model_train.predict(train_data.iloc[[i]]))
+
+    # print(zeries)       # type = series ใช้ predict ไม้ได้  :  Age    22.0 Sex     0.0  Name: 0, dtype: float64
+    # print([zeries])     # type = list                   : [Age    22.0    Sex     0.0 Name: 0, dtype: float64]
+    # print(type(zeries))     # <class 'pandas.core.series.Series'>
+    # print(type([zeries]))  # ตรงนี้ มาเป็น list แต่ใช้ไม่ได้ #   # <class 'list'>
+
+    # print(type([zeries.values]))    # <class 'list'> เช่นกัน
+    # scenario = model_train.predict([zeries.values]) # ทดลอง เอาแทนค่ามารับไว้
+    # print(scenario) # บ่ได้
+
+    #print(zeries.values.reshape(1,-1))
+    #print(type(zeries.values.reshape(1,-1))) # [[22.  0.]] <class 'numpy.ndarray'> ควรจะได้หรือไม่? แต่ว่ามันก็ไม่ได้
+    # spt =  model_train.predict(zeries.values.reshape(1,-1))
+    # print(spt)
+
+    # model_train.predict(train_data.loc[i:]) # จบข่าว 
+    # print(train_data.loc[i])    #   Age    22.0 Sex     0.0 Name: 0, dtype: float64
+    # print(type(train_data.loc[i]))# <class 'pandas.core.series.Series'>
+
+    # i found it! 
+    # print(train_data.loc[i:])   #  Age  Sex    0  22.0    0  [890 rows x 2 columns] หลายตัวไปหน่อย
+    # print(type(train_data.loc[i:]))#    <class 'pandas.core.frame.DataFrame'> เนี่ย มันเป็น Dataframe เว้ย ที่มันต้องการอะ 
+    # print(train_data.iloc[[i]])   #       Age  Sex    0  22.0    0 ตัวเดียวในแต่ละ loop
+    # print(type(train_data.iloc[[i]]))#  <class 'pandas.core.frame.DataFrame'>
+    # model_train.predict(train_data.iloc[[i]])   # its! work
+
+    # gpt give
+    # row = zeries.to_frame().T  # แปลง Series → DataFrame 1 แถว
+    # prediction = model_train.predict(row)
+    # print(prediction)
+
+    # print(type(row))    #   <class 'pandas.core.frame.DataFrame'>
+    # print(row)  #    Age  Sex   0  22.0  0.0
+
+#   check if what wrong :: how ever THIS IS OVERFITTING i mean not sure to be sure
+print(src.drop(6).head())   # everything alright
