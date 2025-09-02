95d65f384e9dd353ce609639e1be1237a51abeeb
InwSaKodBeiw007 <gamekung2707@gmail.com>
2025-09-02 19:56:38 +0700

    touched-HAHAHAH

--- titanic.py
+++ titanic.py
@@ -1,5 +1,7 @@
 import sklearn.datasets as sklearn
 import numpy as np
+from sklearn import tree
+import matplotlib.pyplot as plt
 
 iris =  sklearn.load_iris()
 # print(iris)
@@ -12,4 +14,14 @@ train_data   = iris.data
 # lovePYTHON = [2,3,4]
 # lovePYTHON = np.arange(6)
 # print(type(lovePYTHON))
+testpior = [0,50,100]
+test_data = iris.data[testpior]
 
+model = tree.DecisionTreeClassifier()
+model.fit(train_data,train_target)
+# print(model.predict(test_data))
+
+lemme = model.predict(test_data)
+# print(iris.keys()) #dict_keys(['data', 'target', 'frame', 'target_names', 'DESCR', 'feature_names', 'filename'])
+for i in lemme :
+    print(iris.target_names[i])
