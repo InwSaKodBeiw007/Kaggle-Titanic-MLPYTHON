7ad4d9470be56502fed93e30132b7f8c78cecec3
InwSaKodBeiw007 <gamekung2707@gmail.com>
2025-09-02 18:47:24 +0700

    create of humannity

---
+++ titanic.py
@@ -0,0 +1,15 @@
+import sklearn.datasets as sklearn
+import numpy as np
+
+iris =  sklearn.load_iris()
+# print(iris)
+train_target = iris.target
+train_data   = iris.data
+
+# print(test_target)
+# print(test_data)
+# lovePYTHON = np.zeros((1,3,1))
+# lovePYTHON = [2,3,4]
+# lovePYTHON = np.arange(6)
+# print(type(lovePYTHON))
+
